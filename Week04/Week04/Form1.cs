3ec5c6f8885c248dda1b26cc7f3e6d18b01b3fc4
ritustus <rita.kerkovits@stud.uni-corvinus.hu>
2022-10-05 18:34:50 +0200

    excel

--- Week04/Week04/Form1.cs
+++ Week04/Week04/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
 
 namespace Week04
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
+
+        private void LoadData() 
+        {
+            //Flats nevű Flat típusú elemekból álló listába másolja
+            Flats = context.Flats.ToList();
+        }
+
     }
 }
