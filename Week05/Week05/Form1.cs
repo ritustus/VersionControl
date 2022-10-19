4ef79606c8e61be813bbd58aa1ae0601d4381c50
ritustus <rita.kerkovits@stud.uni-corvinus.hu>
2022-10-19 17:53:52 +0200

    4.

--- Week05/Week05/Form1.cs
+++ Week05/Week05/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week05.Entities;
+using Week05.MNBServiceReference;
 
 namespace Week05
 {
     public partial class Form1 : Form
     {
+
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        
+
         public Form1()
         {
             InitializeComponent();
+
+            dataGridView1.DataSource = Rates;
+
         }
+
+        private void GetExchangeRates() 
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+           
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+        }
+
+       
+        
+
+       
     }
 }
