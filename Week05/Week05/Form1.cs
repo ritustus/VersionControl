1d9a864b679dab842a3c49658baf414ed2cb0967
ritustus <rita.kerkovits@stud.uni-corvinus.hu>
2022-10-19 18:25:57 +0200

    5.

--- Week05/Week05/Form1.cs
+++ Week05/Week05/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Week05.Entities;
 using Week05.MNBServiceReference;
 
@@ -16,17 +17,18 @@ namespace Week05
     {
 
         BindingList<RateData> Rates = new BindingList<RateData>();
-        
+
 
         public Form1()
         {
             InitializeComponent();
-
             dataGridView1.DataSource = Rates;
+            GetExchangeRates();
+            
 
         }
 
-        private void GetExchangeRates() 
+        private void GetExchangeRates()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
@@ -36,11 +38,37 @@ namespace Week05
                 endDate = "2020-06-30"
             };
 
-           
+
 
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(element.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+
+                if (unit != 0)
+                {
+                    rate.Value = value / unit;
+                }
+            }
         }
+
+      
+
+        
 
        
         
