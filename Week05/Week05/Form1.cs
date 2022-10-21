35ee8ed6666fee1ac61121db6565a884973fd2fc
ritustus <rita.kerkovits@stud.uni-corvinus.hu>
2022-10-21 13:00:16 +0200

    last

--- Week05/Week05/Form1.cs
+++ Week05/Week05/Form1.cs
@@ -18,17 +18,22 @@ namespace Week05
     {
 
         BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
 
 
         public Form1()
         {
             InitializeComponent();
-            dataGridView1.DataSource = Rates;
-            GetExchangeRates();
-            chartRateData.DataSource = Rates;
-            RefreshData();
-            comboBox1.Text = "EUR";
+            //dataGridView1.DataSource = Rates;
             
+            
+           // chartRateData.DataSource = Rates;
+            
+            comboBox1.Text = "EUR";
+            comboBox1.DataSource = Currencies;
+            GetCurrenciesRequest();
+           // GetExchangeRates();
+            RefreshData();
 
         }
 
@@ -38,17 +43,19 @@ namespace Week05
             var request = new GetExchangeRatesRequestBody()
             {
                 currencyNames = (string)comboBox1.SelectedItem,
+                //currencyNames = "EUR",
                 startDate = dtmpStart.Value.ToString(),
                 endDate = dtmpEnd.Value.ToString()
             };
 
-
-
             var response = mnbService.GetExchangeRates(request);
+
             var result = response.GetExchangeRatesResult;
 
             var xml = new XmlDocument();
             xml.LoadXml(result);
+
+
 
             foreach (XmlElement element in xml.DocumentElement)
             {
@@ -58,6 +65,10 @@ namespace Week05
                 rate.Date = DateTime.Parse(element.GetAttribute("date"));
 
                 var childElement = (XmlElement)element.ChildNodes[0];
+                if (childElement == null)
+                {
+                    continue;
+                }
                 rate.Currency = childElement.GetAttribute("curr");
 
                 var unit = decimal.Parse(element.GetAttribute("unit"));
@@ -67,30 +78,53 @@ namespace Week05
                 {
                     rate.Value = value / unit;
                 }
-
-
-                var series = chartRateData.Series[0];
-                series.ChartType = SeriesChartType.Line;
-                series.XValueMember = "Date";
-                series.YValueMembers = "Value";
-                series.BorderWidth = 2;
-
-               var legend = chartRateData.Legends[0];
-               legend.Enabled = false;
-
-                var chartArea = chartRateData.ChartAreas[0];
-                chartArea.AxisX.MajorGrid.Enabled = false;
-                chartArea.AxisY.MajorGrid.Enabled = false;
-                chartArea.AxisY.IsStartedFromZero = false;
-
-
             }
+  
+        }
+
+        private void CreateChart()
+        {
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+      
+        private void GetCurrenciesRequest() 
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetCurrenciesRequestBody();
+            var MnbGetExResp = mnbService.GetCurrencies(request);
+            var result = MnbGetExResp.GetCurrenciesResult;
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement x in xml.DocumentElement.ChildNodes[0])
+            {
+                Currencies.Add(x.InnerText);
+            }
+
+
         }
 
         private void RefreshData() 
         {
             Rates.Clear();
-            
+            //comboBox1.DataSource = Currencies;
+            dataGridView1.DataSource = Rates;
+            chartRateData.DataSource = Rates;
+            GetExchangeRates();
+            CreateChart();
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
