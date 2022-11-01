b785a27e51e3c8c212166b6c1ddf27c1b70a2654
ritustus <rita.kerkovits@stud.uni-corvinus.hu>
2022-11-01 15:44:47 +0100

    ballcs

---
+++ Week06/Week06/Entities/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week06.Entities
+{
+    class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Height = 50;
+            Width = 50;
+            Paint += Ball_Paint;
+           
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DrawImage(Graphics graphics)
+        {
+
+        }
+
+        public void MoveBall() 
+        {
+            Left += 1;
+        }
+    }
+}
