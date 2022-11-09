4b6a6716bac32b251953bf2ee9ec09d95a0d5c98
ritustus <rita.kerkovits@stud.uni-corvinus.hu>
2022-11-09 22:11:25 +0100

    portfolio value

--- Week07/Week07/Form1.cs
+++ Week07/Week07/Form1.cs
@@ -7,14 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week07.Entities;
 
 namespace Week07
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+            
         }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+
+        private decimal GetPortfolioValue(DateTime date) 
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim() //a Trim()-el levágjuk a felesleges karaktereket amikkel fel van töltve az index mező
+                            && date <= x.TradingDay
+                            select x).First();
+                value += (decimal)last.Price * item.Volume;
+            }
+
+            return value;
+        }
+
+
     }
 }
