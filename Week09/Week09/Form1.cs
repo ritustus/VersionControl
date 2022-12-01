adacd07aa3a424a245478021850e79185a9071c5
ritustus <rita.kerkovits@stud.uni-corvinus.hu>
2022-12-01 21:32:10 +0100

    nem vágom

--- Week09/Week09/Form1.cs
+++ Week09/Week09/Form1.cs
@@ -17,7 +17,7 @@ namespace Week09
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
-        Random rng = new Random(1500);
+        Random rng = new Random(1234);
 
         public Form1()
         {
