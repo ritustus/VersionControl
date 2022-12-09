633eb0e5992d49a6361fb4688182b79d3fbdbd02
Rita Margit Kerkovits <rita.kerkovits@stud.uni-corvinus.hu>
2022-12-09 12:18:34 +0100

    ende

--- Week10/Week10/Form1.Designer.cs
+++ Week10/Week10/Form1.Designer.cs
@@ -29,13 +29,46 @@ namespace Week10
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bn_start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bn_start
+            // 
+            this.bn_start.Location = new System.Drawing.Point(25, 395);
+            this.bn_start.Name = "bn_start";
+            this.bn_start.Size = new System.Drawing.Size(84, 30);
+            this.bn_start.TabIndex = 0;
+            this.bn_start.Text = "Start";
+            this.bn_start.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 404);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(673, 437);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bn_start);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bn_start;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Week10/Week10/Form1.cs
+++ Week10/Week10/Form1.cs
@@ -7,14 +7,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Week10
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
+
+            bn_start.Visible = false;
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+
+            /* gc.AddPlayer();
+            gc.Start(true);
+           */
+
+
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+
+            label1.BringToFront();
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+
+                bn_start.Visible = true;
+
+                return;
+            }
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            gc.Start(true);
+
+            ga.Focus();
+
+
+        }
+
+        
     }
 }
